1b3824c359c86e9ff90ad0e19b723c0e8aefbbe4
Jack17kdb <jack@kali.Johnson.com>
2026-03-23 09:03:19 +0300

    full project

--- backend/src/server.js
+++ backend/src/server.js
@@ -30,7 +30,7 @@ app.use(helmet({
 			"default-src": ["'self'"],
 			"img-src": ["'self'", "data:", "https://res.cloudinary.com", "https://images.unsplash.com", "https://i.pravatar.cc"],
 			"script-src": ["'self'", "'unsafe-inline'"],
-			"connect-src": ["'self'", "data:", "https://res.cloudinary.com", "https://images.unsplash.com", "https://i.pravatar.cc", "https://kinap.onrender.com"],
+			"connect-src": ["'self'", "data:", "https://res.cloudinary.com", "https://images.unsplash.com", "https://i.pravatar.cc", "https://kinap.onrender.com", "wss://kinap.onrender.com"],
 		},
 	},
 }));
